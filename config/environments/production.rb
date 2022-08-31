62acbf9b4d7897fe23079202ed576ae952645cb9
delfos <rafael.costa@delfosim.com>
2022-08-31 13:47:06 -0300

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  host = 'http://kamarote-api.infinitysolutionsapp.com' #replace with your own url
+  host = 'http://kamarote-api.infinitysolutionsapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
   
   # SMTP settings for gmail
