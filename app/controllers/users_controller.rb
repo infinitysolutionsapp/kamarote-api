f385cbd5eb4ec96b5d1a0aecc4d677a57bb3ce9e
denisnead <denis.silva@unifametro.edu.br>
2022-09-15 22:51:03 -0300

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,28 @@ class UsersController < ApplicationController
   
     # GET /users/{username}
     def show
-      render json: @user, status: :ok
+      render json: {user: @user, account: Account.where(user_id: @user.id)}, status: :ok
+    end
+
+    def me
+      header = request.headers['Authorization']
+      header = header.split(' ').last if header
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+      
+      render json: {user: @current_user, account: Account.where(user_id: @current_user.id)}, status: :ok
     end
   
+    # params.require(:account).permit(:balance, :user_id, :agency, :account)/////////////////////////////////
+
     # POST /users
     def create
       @user = User.new(user_params)
+
       if @user.save
+        @account = Account.new({ balance: 0, agency: "teste" , account: "teste", user_id: @user.id})
+        @account.save
+
         render json: @user, status: :created
       else
         render json: { errors: @user.errors.full_messages },
